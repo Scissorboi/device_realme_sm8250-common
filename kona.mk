1ae7776f7e9ca4e178cf9dbb95872f5a9b092195
dataoutputstream <mb150996@gmail.com>
2020-11-05 22:30:52 +0100

    Cut hostapd Now We don't need aosp, broke hotspopt wifi

--- kona.mk
+++ kona.mk
@@ -87,10 +87,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
-# HOSTPAD
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hostapd:system/bin/hw/hostapd
-
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl.kona
